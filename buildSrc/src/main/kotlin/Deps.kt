89043b97b4aa8ec88b129b7707e4ff721a53c1a1
arkon <eugcheung94@gmail.com>
2020-11-29 23:04:06 -0500

    Update OkHttp to match app

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -2,7 +2,6 @@ object Deps {
 
   object kotlin {
     const val version = "1.4.10"
-    const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib:$version"
   }
 
   const val autoservice = "com.google.auto.service:auto-service:1.0-rc4"
@@ -11,7 +10,7 @@ object Deps {
   const val tachiyomiCommon = "tachiyomi.sourceapi:common:1.1"
   const val tachiyomiSourceApi = "tachiyomi.sourceapi:source-api:1.1"
 
-  const val okhttp = "com.squareup.okhttp3:okhttp:4.6.0"
+  const val okhttp = "com.squareup.okhttp3:okhttp:4.10.0-RC1"
   const val jsoup = "org.jsoup:jsoup:1.13.1"
   const val gson = "com.google.code.gson:gson:2.8.5"
   const val kotson = "com.github.salomonbrys.kotson:kotson:2.5.0"
