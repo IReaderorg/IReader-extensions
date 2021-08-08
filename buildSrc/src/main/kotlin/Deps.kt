153ff551e4dc712588d4b579fc5825d4d6379dfd
arkon <eugcheung94@gmail.com>
2021-08-08 11:24:10 -0400

    Remove jcenter

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -1,14 +1,14 @@
 object Deps {
 
   object kotlin {
-    const val version = "1.4.10"
+    const val version = "1.5.21"
   }
 
   const val autoservice = "com.google.auto.service:auto-service:1.0-rc4"
   const val kotlinpoet = "com.squareup:kotlinpoet:1.0.0"
 
-  const val tachiyomiCommon = "tachiyomi.sourceapi:common:1.1"
-  const val tachiyomiSourceApi = "tachiyomi.sourceapi:source-api:1.1"
+  const val tachiyomiCommon = "org.tachiyomi:common:1.1"
+  const val tachiyomiSourceApi = "org.tachiyomi:source-api:1.1"
 
   const val okhttp = "com.squareup.okhttp3:okhttp:4.10.0-RC1"
   const val jsoup = "org.jsoup:jsoup:1.13.1"
