6d3d2c7f9f3d37f97aa946eb35cc9b704b0cd609
arkon <eugcheung94@gmail.com>
2021-08-08 10:59:50 -0400

    Align SDK versions with app

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -1,5 +1,5 @@
 object Config {
-  const val compileSdk = 30
-  const val minSdk = 21
-  const val targetSdk = 30
+  const val minSdk = 23
+  const val compileSdk = 31
+  const val targetSdk = 31
 }
