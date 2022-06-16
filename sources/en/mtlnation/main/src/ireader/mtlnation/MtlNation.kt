e0af01bf485d5daa7536da357386a3ea79607a66
KazemM <76521159+kazemcodes@users.noreply.github.com>
2022-06-16 11:39:59 +0430

    add two new sources kiss novel and novel hall by @Nabeelshar

--- sources/en/mtlnation/main/src/ireader/mtlnation/MtlNation.kt
+++ sources/en/mtlnation/main/src/ireader/mtlnation/MtlNation.kt
@@ -5,16 +5,14 @@ import io.ktor.client.call.*
 import io.ktor.client.engine.okhttp.*
 import io.ktor.client.plugins.*
 import io.ktor.client.request.*
+import io.ktor.client.request.forms.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import okhttp3.OkHttpClient
 
 import org.ireader.core_api.http.okhttp
 import org.ireader.core_api.log.Log
-import org.ireader.core_api.source.Dependencies
-import org.ireader.core_api.source.ParsedHttpSource
-import org.ireader.core_api.source.asJsoup
-import org.ireader.core_api.source.findInstance
+import org.ireader.core_api.source.*
 import org.ireader.core_api.source.model.*
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
@@ -22,303 +20,156 @@ import org.jsoup.nodes.Element
 import tachiyomix.annotations.Extension
 import java.util.concurrent.TimeUnit
 
-//I haven't finished configuring this extension because of its aggressive cloudflare protecion
 @Extension
-abstract class MtlNation(private val deps: Dependencies) : ParsedHttpSource(deps) {
+abstract class MtlNation(private val deps: Dependencies) : SourceFactory(
+    deps = deps,
+) {
 
-    override val name = "MtlNation"
-
-
+    override val lang: String
+        get() = "en"
+    override val baseUrl: String
+        get() = "https://mtlnation.com"
     override val id: Long
         get() = 7
-    override val baseUrl = "https://mtlnation.com"
-    val translatorUrl = "https://mtlnation-com.translate.goog"
-    val translatorEndPoint = "?_x_tr_sl=auto&_x_tr_tl=en&_x_tr_hl=en-US&_x_tr_pto=op,wapp"
+    override val name: String
+        get() = "MtlNation"
 
-    override val lang = "en"
-
-
-    override fun getFilters(): FilterList {
-        return listOf(
-            Filter.Title(),
-            Filter.Sort(
-                "Sort By:", arrayOf(
-                    "Latest",
-                    "Popular"
-                )
-            ),
-        )
-    }
-
-    private fun clientBuilder(): OkHttpClient = deps.httpClients.default.okhttp
-        .newBuilder()
-        .connectTimeout(10, TimeUnit.SECONDS)
-        .readTimeout(30, TimeUnit.SECONDS)
-        .build()
-
-    override val client = HttpClient(OkHttp) {
-        engine {
-            preconfigured = clientBuilder()
-        }
-    }
-
-
-    override fun getListings(): List<Listing> {
-        return listOf(
-            LatestListing(),
-        )
-    }
-
-    class LatestListing() : Listing("Latest")
-
-    override fun getCoverRequest(url: String): Pair<HttpClient, HttpRequestBuilder> {
-        return client to HttpRequestBuilder(url).apply {
-            url(url)
-            headers {
-                append(
-                    HttpHeaders.UserAgent,
-                    agent
-                )
-                append(HttpHeaders.CacheControl, "max-age=0")
-                append(HttpHeaders.Referrer, translatorUrl)
-            }
-        }
-    }
-
-
-    override suspend fun getMangaList(sort: Listing?, page: Int): MangasPageInfo {
-        return getLatest(page)
-    }
-
-    override suspend fun getMangaList(filters: FilterList, page: Int): MangasPageInfo {
-        val sorts = filters.findInstance<Filter.Sort>()?.value?.index
-        val query = filters.findInstance<Filter.Title>()?.value
-        if (!query.isNullOrBlank()) {
-            return getSearch(page, query)
-        }
-        return when (sorts) {
-            0 -> getLatest(page)
-            1 -> getPopular(page)
-            else -> getLatest(page)
-        }
-    }
-
-    override fun getCommands(): CommandList {
-        return listOf(
-            Command.Chapter.Note("you can fetch chapters by going to WebView and click on Fetch Chapters "),
-            Command.Chapter.Fetch(),
-            Command.Content.Fetch(),
-            Command.Detail.Fetch()
-        )
-    }
-
-    override suspend fun getMangaDetails(manga: MangaInfo, commands: List<Command<*>>): MangaInfo {
-        val command = commands.find { it is Command.Detail.Fetch }
-        if (command != null && command is Command.Detail.Fetch) {
-            return detailParsed(Jsoup.parse(command.html), command.url)
-        }
-        val html = client.get(requestBuilder(manga.key))
-
-        return detailParse(html.asJsoup())
-    }
-
-    override suspend fun getPageList(chapter: ChapterInfo, commands: List<Command<*>>): List<Page> {
-        val command = commands.find { it is Command.Content.Fetch }
-        if (command != null && command is Command.Content.Fetch) {
-            return pageContentParse(Jsoup.parse(command.html)).map { Text(it) }
-        }
-        val url = chapter.key.replace(
-            baseUrl,
-            translatorUrl
-        ) + "?_x_tr_sl=auto&_x_tr_tl=en&_x_tr_hl=en-US&_x_tr_pto=op,wapp"
-        val html = client.get(requestBuilder(url = url))
-        return pageContentParse(html.asJsoup()).map { Text(it) }
-    }
-
-    private val agent =
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"
-
-    suspend fun getLatest(page: Int): MangasPageInfo {
-        val html: HttpResponse =
-            client.get(requestBuilder("https://mtlnation-com.translate.goog/novel/page/$page/?m_orderby=latest&_x_tr_sl=auto&_x_tr_tl=en&_x_tr_hl=en-US&_x_tr_pto=wapp"))
-
-        return bookListParse(
-            html.asJsoup(),
-            "div.page-item-detail",
-            ".last"
-        ) { popularFromElement(it) }
-    }
-
-
-    suspend fun getPopular(page: Int): MangasPageInfo {
-        val response =client.get(
-            requestBuilder("https://mtlnation-com.translate.goog/novel/page/$page/?m_orderby=trending&_x_tr_sl=auto&_x_tr_tl=fa&_x_tr_hl=en-US&_x_tr_pto=op,wapp",)
-        )
-        return bookListParse(
-            response.asJsoup(),
-            "div.page-item-detail",
-            "a.last"
-        ) { popularFromElement(it) }
-    }
-
-    suspend fun getSearch(page: Int, query: String): MangasPageInfo {
-        val response: HttpResponse =
-            client.get(requestBuilder("https://mtlnation-com.translate.goog/?s=$query&post_type=wp-manga&_x_tr_sl=auto&_x_tr_tl=fa&_x_tr_hl=en-US&_x_tr_pto=op,wapp"))
-
-        return bookListParse(
-            response.asJsoup(),
-            ".c-tabs-item__content",
-            null
-        ) { searchFromElement(it) }
-    }
-
-
-    override fun HttpRequestBuilder.headersBuilder(block: HeadersBuilder.() -> Unit) {
-        headers {
-            append(
-                HttpHeaders.UserAgent,
-                "Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.91 Mobile Safari/537.36"
+    override fun getFilters(): FilterList = listOf(
+        Filter.Title(),
+        Filter.Sort(
+            "Sort By:", arrayOf(
+                "Latest",
+                "Popular",
+                "New",
+                "Most Views",
+                "Rating",
             )
-            append(HttpHeaders.CacheControl, "max-age=0")
-            append(HttpHeaders.Referrer, baseUrl)
-        }
-    }
+        ),
+    )
 
+    override val exploreFetchers: List<BaseExploreFetcher>
+        get() = listOf(
+            BaseExploreFetcher(
+                "Latest",
+                endpoint = "/manga/page/{page}/?m_orderby=latest",
+                selector = ".card ",
+                nameSelector = "a",
+                nameAtt = "title",
+                linkSelector = "a",
+                linkAtt = "href",
+                coverSelector = "a img",
+                coverAtt = "src",
+                nextPageSelector = ".nav-previous",
+                nextPageValue = "Older Posts"
+            ),
+            BaseExploreFetcher(
+                "Search",
+                endpoint = "/page/{page}/?s={query}&post_type=wp-manga",
+                selector = ".c-tabs-item .row",
+                nameSelector = "a",
+                linkSelector = "a",
+                linkAtt = "href",
+                coverSelector = "a img",
+                coverAtt = "src",
+                nextPageSelector = ".nav-previous",
+                nextPageValue = "Older Posts",
+                type = Type.Search
+            ),
+            BaseExploreFetcher(
+                "Trending",
+                endpoint = "/manga/page/{page}/?m_orderby=trending",
+                selector = ".page-item-detail .item-thumb",
+                nameSelector = "a",
+                nameAtt = "title",
+                linkSelector = "a",
+                linkAtt = "href",
+                coverSelector = "a img",
+                coverAtt = "src",
+                nextPageSelector = ".nav-previous",
+                nextPageValue = "Older Posts"
+            ),
+            BaseExploreFetcher(
+                "New",
+                endpoint = "/manga/page/{page}/?m_orderby=new-manga",
+                selector = ".page-item-detail .item-thumb",
+                nameSelector = "a",
+                nameAtt = "title",
+                linkSelector = "a",
+                linkAtt = "href",
+                coverSelector = "a img",
+                coverAtt = "src",
+                nextPageSelector = ".nav-previous",
+                nextPageValue = "Older Posts"
+            ),
+            BaseExploreFetcher(
+                "Most Views",
+                endpoint = "/manga/page/{page}/?m_orderby=views",
+                selector = ".page-item-detail .item-thumb",
+                nameSelector = "a",
+                nameAtt = "title",
+                linkSelector = "a",
+                linkAtt = "href",
+                coverSelector = "a img",
+                coverAtt = "src",
+                nextPageSelector = ".nav-previous",
+                nextPageValue = "Older Posts"
+            ),
+            BaseExploreFetcher(
+                "Rating",
+                endpoint = "/manga/page/{page}/?m_orderby=rating",
+                selector = ".page-item-detail .item-thumb",
+                nameSelector = "a",
+                nameAtt = "title",
+                linkSelector = "a",
+                linkAtt = "href",
+                coverSelector = "a img",
+                coverAtt = "src",
+                nextPageSelector = ".nav-previous",
+                nextPageValue = "Older Posts"
+            ),
 
-    fun popularFromElement(element: Element): MangaInfo {
+            )
 
-        val url = element.select("h3.h5 a").attr("href").replace(translatorUrl,baseUrl)
-        val title = element.select("h3.h5 a").text()
-        val thumbnailUrl = element.select("img").attr("data-src")
-        return MangaInfo(key = url, title = title, cover = thumbnailUrl)
-    }
-
-
-    fun searchFromElement(element: Element): MangaInfo {
-        val title = element.select("a").text()
-        val url = baseUrl + element.select("a").attr("href")
-        val thumbnailUrl = element.select("img").attr("data-src")
-        return MangaInfo(key = url, title = title, cover = thumbnailUrl)
-    }
-
-    // manga details
-    fun detailParsed(document: Document, url: String): MangaInfo {
-        val title = document.select(".post-title h1").text()
-        val cover = document.select(".summary_image img").attr("data-src")
-        val link = url.ifBlank { document.select(".summary_image a").attr("href") }
-        val authorBookSelector = document.select(".author-content a").text()
-        val description = document.select(".summary__content p").eachText().joinToString("\n")
-        //not sure why its not working.
-        val category = document.select(".genres-content a")
-            .eachText()
-
-        val status = document.select(".post-status .summary-content")
-            .text()
-            .handleStatus()
-
-
-
-
-        return MangaInfo(
-            title = title,
-            cover = cover,
-            description = description,
-            author = authorBookSelector,
-            genres = category,
-            key = link,
-            status = status
+    override val detailFetcher: Detail
+        get() = SourceFactory.Detail(
+            nameSelector = ".post-title",
+            coverSelector = ".summary_image a img",
+            coverAtt = "src",
+            authorBookSelector = ".author-content a",
+            categorySelector = ".genres-content a",
+            descriptionSelector = ".g_txt_over p",
         )
-    }
 
-    override fun detailParse(document: Document): MangaInfo {
-        val title = document.select(".post-title h1").text()
-        val cover = document.select(".summary_image img").attr("data-src")
-        val link = document.select(".summary_image a").attr("href")
-        val authorBookSelector = document.select(".author-content a").text()
-        val description = document.select(".summary__content p").eachText().joinToString("\n")
-        //not sure why its not working.
-        val category = document.select(".genres-content a")
-            .eachText()
-
-        val status = document.select(".post-status .summary-content")
-            .text()
-            .handleStatus()
-
-
-
-
-        return MangaInfo(
-            title = title,
-            cover = cover,
-            description = description,
-            author = authorBookSelector,
-            genres = category,
-            key = link,
-            status = status
+    override val chapterFetcher: Chapters
+        get() = SourceFactory.Chapters(
+            selector = ".wp-manga-chapter",
+            nameSelector = "a",
+            linkSelector = "a",
+            linkAtt = "href",
         )
-    }
 
-    private fun String.handleStatus(): Int {
-        return when (this) {
-            "OnGoing" -> MangaInfo.ONGOING
-            "Complete" -> MangaInfo.COMPLETED
-            else -> MangaInfo.ONGOING
-        }
-
-    }
-
-    // chapters
-    override fun chaptersRequest(book: MangaInfo): HttpRequestBuilder {
-        return HttpRequestBuilder().apply {
-            url(book.key)
-            headers { headers }
-        }
-    }
-
-    override fun chaptersSelector() = ".wp-manga-chapter a"
-
-    override fun chapterFromElement(element: Element): ChapterInfo {
-        val link = element.select("a").attr("href")
-        val name = element.select("a").text()
-
-        return ChapterInfo(name = name, key = link)
-    }
+    override val contentFetcher: Content
+        get() = SourceFactory.Content(
+            pageTitleSelector = ".cha-tit",
+            pageContentSelector = ".text-left h3,p ,.cha-content .pr .dib p",
+        )
 
 
     override suspend fun getChapterList(
         manga: MangaInfo,
         commands: List<Command<*>>
     ): List<ChapterInfo> {
-        val command = commands.find { it is Command.Chapter.Fetch }
-        if (command != null && command is Command.Chapter.Fetch) {
-            return chaptersParse(Jsoup.parse(command.html)).reversed()
-        }
-
-        val url = manga.key
-        val html = deps.httpClients.browser.fetch(
-            url,
-            chaptersSelector(),
-            timeout = 50000,
-            userAgent = agent
-        ).responseBody
-        return chaptersParse(html.asJsoup()).reversed()
-    }
+        val html = client.get(requestBuilder(manga.key)).asJsoup()
+        val bookId = html.select(".rating-post-id").attr("value")
 
 
-    override fun pageContentParse(document: Document): List<String> {
-        return document.select("div.txt h4,p").eachText().drop(1)
-    }
-
-    override suspend fun getContents(chapter: ChapterInfo): List<String> {
-        return pageContentParse(client.get(contentRequest(chapter)).asJsoup())
-    }
-
-
-    override fun contentRequest(chapter: ChapterInfo): HttpRequestBuilder {
-        return HttpRequestBuilder().apply {
-            url(chapter.key)
-            headers { headers }
-        }
+        var chapters = chaptersParse(
+            client.submitForm(url = "https://skynovel.org/wp-admin/admin-ajax.php", formParameters = Parameters.build {
+                append("action", "manga_get_chapters")
+                append("manga", bookId)
+            }).asJsoup(),
+        )
+        return chapters.reversed()
     }
 
 
---
+++ sources/en/novelhall/main/src/ireader/novelhall/NovelHall.kt
@@ -0,0 +1,249 @@
+package ireader.novelhall
+
+import io.ktor.client.request.*
+import io.ktor.http.*
+import org.ireader.core_api.source.*
+import org.ireader.core_api.source.model.*
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import tachiyomix.annotations.Extension
+
+@Extension
+abstract class NovelHall(deps: Dependencies) : ParsedHttpSource(deps) {
+
+    override val name = "NovelHall"
+
+
+    override val id: Long
+        get() = 27
+    override val baseUrl = "https://www.novelhall.com"
+
+    override val lang = "en"
+
+
+    override fun getFilters(): FilterList {
+        return listOf(
+            Filter.Title(),
+            Filter.Sort(
+                "Sort By:", arrayOf(
+                    "Latest",
+                    "Popular"
+                )
+            ),
+        )
+    }
+
+    override fun getListings(): List<Listing> {
+        return listOf(
+            SourceFactory.LatestListing(),
+        )
+    }
+
+    override suspend fun getMangaList(sort: Listing?, page: Int): MangasPageInfo {
+        return getLatest(page)
+    }
+
+    override suspend fun getMangaList(filters: FilterList, page: Int): MangasPageInfo {
+        val sorts = filters.findInstance<Filter.Sort>()?.value?.index
+        val query = filters.findInstance<Filter.Title>()?.value
+        if (!query.isNullOrBlank()) {
+            return getSearch(query, page)
+        }
+        return when (sorts) {
+            0 -> getLatest(page)
+            1 -> getPopular(page)
+            else -> getLatest(page)
+        }
+    }
+
+    suspend fun getLatest(page: Int): MangasPageInfo {
+        val res = requestBuilder(baseUrl + fetchLatestEndpoint(page))
+        return bookListParse(
+            client.get(res).asJsoup(),
+            latestSelector(),
+            latestNextPageSelector()
+        ) { latestFromElement(it) }
+    }
+
+    suspend fun getPopular(page: Int): MangasPageInfo {
+        val res = requestBuilder(baseUrl + fetchPopularEndpoint(page))
+        return bookListParse(
+            client.get(res).asJsoup(),
+            popularSelector(),
+            popularNextPageSelector()
+        ) { popularFromElement(it) }
+    }
+
+    suspend fun getSearch(query: String, page: Int): MangasPageInfo {
+        val res = requestBuilder("$baseUrl/index.php?s=so&module=book&keyword=$query")
+        return bookListParse(
+            client.get(res).asJsoup(),
+            ".section3 table tr",
+            null
+        ) { searchFromElement(it) }
+    }
+
+
+    fun fetchLatestEndpoint(page: Int): String? =
+        "/all-$page.html"
+
+    fun fetchPopularEndpoint(page: Int): String? =
+        "/all-$page.html"
+
+//     fun fetchSearchEndpoint(page: Int, query: String): String? =
+//        "/index.php?s=so&module=book&keyword=$page"
+
+
+    override fun HttpRequestBuilder.headersBuilder(block: HeadersBuilder.() -> Unit) {
+        headers {
+            append(
+                HttpHeaders.UserAgent,
+                "Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.91 Mobile Safari/537.36"
+            )
+            append(HttpHeaders.CacheControl, "max-age=0")
+            append(HttpHeaders.Referrer, baseUrl)
+        }
+    }
+
+
+    fun popularSelector() = ".type li.btm"
+
+    fun popularFromElement(element: Element): MangaInfo {
+        val url = baseUrl + element.select(".btm a").attr("href")
+        val title = element.select("li.btm a").text()
+        val thumbnailUrl = baseUrl + element.select(".col-xs-3 img").attr("src")
+        return MangaInfo(key = url, title = title, cover = thumbnailUrl)
+    }
+
+    fun popularNextPageSelector() = ".container"
+    fun searchFromElement(element: Element): MangaInfo {
+        val title = element.select("tr > td:nth-child(2) > a").text()
+        val url = baseUrl + element.select("tr > td:nth-child(2) > a").attr("href")
+        val thumbnailUrl = element.select("div.pic img").attr("src")
+        return MangaInfo(key = url, title = title, cover = thumbnailUrl)
+    }
+
+    // latest
+
+    fun latestRequest(page: Int): HttpRequestBuilder {
+        return HttpRequestBuilder().apply {
+            url(baseUrl + fetchLatestEndpoint(page)!!)
+            headers { headers }
+        }
+    }
+
+    fun latestSelector(): String = popularSelector()
+
+
+    fun latestFromElement(element: Element): MangaInfo = popularFromElement(element)
+
+    fun latestNextPageSelector() = popularNextPageSelector()
+
+
+    fun searchNextPageSelector(): String? = popularNextPageSelector()
+
+
+    // manga details
+    override fun detailParse(document: Document): MangaInfo {
+        val title = document.select(".book-info h1").text()
+        val authorBookSelector =
+            document.select(".book-info .blue:nth-child(2)").text().replace("Author：", "")
+                .replace(".p", "").replace("0", "")
+        val cover = document.select(".book-img.hidden-xs img").attr("src")
+        val description = document.select("span.js-close-wrap").eachText().joinToString("\n")
+            .replace("back<<", "")
+        //not sure why its not working.
+        val category = document.select("a.red")
+            .eachText()
+
+        val status = document.select(".book-info .blue:nth-child(3)")
+            .next()
+            .text()
+            .replace("Status：Active", "OnGoing")
+            .replace("Status：", "")
+            .handleStatus()
+
+
+
+
+        return MangaInfo(
+            title = title,
+            cover = cover,
+            description = description,
+            author = authorBookSelector ?: "",
+            genres = category,
+            key = "",
+            status = status
+        )
+    }
+
+    private fun String.handleStatus(): Int {
+        return when (this) {
+            "OnGoing" -> MangaInfo.ONGOING
+            "Complete" -> MangaInfo.COMPLETED
+            else -> MangaInfo.ONGOING
+        }
+
+    }
+
+    // chapters
+    override fun chaptersRequest(book: MangaInfo): HttpRequestBuilder {
+        return HttpRequestBuilder().apply {
+            url(book.key)
+            headers { headers }
+        }
+    }
+
+    override fun chaptersSelector() = "#morelist a"
+
+    override fun chapterFromElement(element: Element): ChapterInfo {
+        val link = baseUrl + element.select("a").attr("href")
+        val name = element.select("a").text()
+
+        return ChapterInfo(name = name, key = link)
+    }
+
+    private fun uniqueChaptersRequest(book: MangaInfo, page: Int): HttpRequestBuilder {
+        return HttpRequestBuilder().apply {
+            url(
+                book.key + "?page=$page"
+            )
+            headers { headers }
+        }
+    }
+
+    override suspend fun getChapterList(
+        manga: MangaInfo,
+        commands: List<Command<*>>
+    ): List<ChapterInfo> {
+        val request = client.get(chaptersRequest(manga)).asJsoup()
+        return chaptersParse(request).reversed()
+    }
+
+    suspend fun parseMaxPage(book: MangaInfo): Int {
+        val page = client.get(chaptersRequest(book = book)).asJsoup()
+        val maxPage = page.select("li.last > a").attr("data-page")
+        return maxPage.toInt()
+    }
+
+
+    override fun pageContentParse(document: Document): List<String> {
+        return document.select("div#htmlContent").html().split("<br>", "<p>")
+            .map { Jsoup.parse(it).text() }
+    }
+
+    override suspend fun getContents(chapter: ChapterInfo): List<String> {
+        return pageContentParse(client.get(contentRequest(chapter)).asJsoup())
+    }
+
+
+    override fun contentRequest(chapter: ChapterInfo): HttpRequestBuilder {
+        return HttpRequestBuilder().apply {
+            url(chapter.key)
+            headers { headers }
+        }
+    }
+
+
+}
