4d2edad81971d741bc3f8f83e50a272c9731a716
malav patel <35185886+patel-malav@users.noreply.github.com>
2023-03-20 11:42:13 -0400

    Fix extension fastnovel (#62)
    * Fixed Extension Fastnovel
    
    * Fix Status Selector

--- sources/en/fastnovel/main/src/ireader/fastnovel/FastNovel.kt
+++ sources/en/fastnovel/main/src/ireader/fastnovel/FastNovel.kt
@@ -1,12 +1,15 @@
 package ireader.fastnovel
 
 import ireader.core.source.Dependencies
+import ireader.core.source.SourceFactory
 import ireader.core.source.model.Command
 import ireader.core.source.model.CommandList
 import ireader.core.source.model.Filter
 import ireader.core.source.model.FilterList
-import ireader.core.source.SourceFactory
+import ireader.core.source.model.MangaInfo
+import org.jsoup.nodes.Document
 import tachiyomix.annotations.Extension
+
 
 @Extension
 abstract class FastNovel(private val deps: Dependencies) : SourceFactory(
@@ -16,7 +19,7 @@ abstract class FastNovel(private val deps: Dependencies) : SourceFactory(
     override val lang: String
         get() = "en"
     override val baseUrl: String
-        get() = "https://fastnovel.net"
+        get() = "https://fastnovel.org"
     override val id: Long
         get() = 40
     override val name: String
@@ -38,54 +41,81 @@ abstract class FastNovel(private val deps: Dependencies) : SourceFactory(
         get() = listOf(
             BaseExploreFetcher(
                 "Trending",
-                endpoint = "/list/most-popular.html?page={page}",
-                selector = ".film-item",
-                nameSelector = ".name",
-                coverSelector = ".img",
-                coverAtt = "data-original",
-                linkSelector = "a",
+                endpoint = "/sort/p?page={page}",
+                selector = ".col-novel-main > .list-novel .row",
+                nameSelector = ".novel-title",
+                coverSelector = ".cover",
+                coverAtt = "src",
+                linkSelector = ".novel-title > a",
                 linkAtt = "href",
                 maxPage = 39,
-                addBaseUrlToLink = true
+                addBaseUrlToLink = false,
+                onCover = { url, _ -> url.replace(Regex("/novel[\\s\\S]*/"), "/novel/") }
             ),
             BaseExploreFetcher(
                 "Search",
-                endpoint = "/search/{query}",
-                selector = ".film-item",
-                nameSelector = "div.title > p.name",
-                coverSelector = ".img",
-                coverAtt = "data-original",
-                linkSelector = "a",
+                endpoint = "/search?keyword={query}",
+                selector = ".col-novel-main > .list-novel .row",
+                nameSelector = ".novel-title",
+                coverSelector = ".cover",
+                coverAtt = "src",
+                linkSelector = ".novel-title > a",
                 linkAtt = "href",
-                addBaseUrlToLink = true,
+                addBaseUrlToLink = false,
                 type = SourceFactory.Type.Search
             ),
-
         )
 
     override val detailFetcher: Detail
         get() = SourceFactory.Detail(
-            nameSelector = "h1",
-            coverSelector = ".book-cover",
-            coverAtt = "data-original",
-            authorBookSelector = "li > label",
-            categorySelector = "div.film-info > ul > li:nth-child(2) > a",
-            descriptionSelector = "div.film-content > p",
-            statusSelector = "li",
+            nameSelector = ".info-holder .title",
+            coverSelector = ".book > img",
+            coverAtt = "src",
+            // We are using nth-last-child(x) because there are some
+            // novels which can have alternative names that is added
+            // on top of this .info list
+            authorBookSelector = ".info > li:nth-last-child(4) a",
+            categorySelector = ".info > li:nth-last-child(3) a",
+            descriptionSelector = ".desc-text",
+            statusSelector = ".info > li:nth-last-child(1) a",
+            onStatus = { str ->
+                when (str) {
+                    "Completed" -> MangaInfo.COMPLETED
+                    "Ongoing" -> MangaInfo.ONGOING
+                    else -> MangaInfo.UNKNOWN
+                }
+            }
         )
 
     override val chapterFetcher: Chapters
         get() = SourceFactory.Chapters(
-            selector = ".chapter",
-            nameSelector = ".chapter",
-            linkSelector = ".chapter",
+            selector = ".list-chapter > li",
+            reverseChapterList = true,
+            nameSelector = "a",
+            nameAtt = "title",
+            linkSelector = "a",
             linkAtt = "href",
-            addBaseUrlToLink = true
+            addBaseUrlToLink = false,
+            numberSelector = "a",
+            numberAtt = "title",
+            onNumber = { str -> str.substringAfter("Chapter ").substringBefore(" ") }
         )
 
     override val contentFetcher: Content
         get() = SourceFactory.Content(
-            pageTitleSelector = ".episode-name",
-            pageContentSelector = "#chapter-body p",
+            pageTitleSelector = ".chr-title > .chr-text",
+            pageContentSelector = "#chr-content > p",
         )
+
+    override suspend fun getChapterListRequest(
+        manga: MangaInfo,
+        commands: List<Command<*>>
+    ): Document {
+        val id = manga.key.substringAfterLast("/")
+        val updatedManga = MangaInfo(
+            key = "https://fastnovel.org/ajax/chapter-archive?novelId=$id",
+            title = manga.title,
+        )
+        return super.getChapterListRequest(updatedManga, commands)
+    }
 }
