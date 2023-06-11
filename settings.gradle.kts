7cfd09e1f6131c65b561d631077686cac93f88ff
kazem <kazem.codes@gmail.com>
2023-06-11 15:12:04 +0330

    batch extension workflow

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,25 +14,39 @@ include(":compiler")
 include(":deeplink")
 include(":defaultRes")
 
-
-File(rootDir, "sources").eachDir { dir ->
-    if (dir.name != "multisrc") {
-        dir.eachDir { subDir ->
-            if (File(subDir, "build.gradle.kts").exists()) {
-                val name = ":extensions:individual:${dir.name}:${subDir.name}"
-                include(name)
-                project(name).projectDir = File("sources/${dir.name}/${subDir.name}")
+if (System.getenv("CI") == null || System.getenv("CI_MODULE_GEN") == "true") {
+    File(rootDir, "sources").eachDir { dir ->
+        if (dir.name != "multisrc") {
+            dir.eachDir { subDir ->
+                if (File(subDir, "build.gradle.kts").exists()) {
+                    val name = ":extensions:individual:${dir.name}:${subDir.name}"
+                    include(name)
+                    project(name).projectDir = File("sources/${dir.name}/${subDir.name}")
+                }
             }
         }
     }
-}
 
-File(rootDir, "sources/multisrc").eachDir { dir ->
-    if (File(dir, "build.gradle.kts").exists()) {
-        val dirName = ":extensions:multisrc:${dir.name}"
-        include(dirName)
-        project(dirName).projectDir =
-            File("sources/multisrc/${dir.name}")
+    File(rootDir, "sources/multisrc").eachDir { dir ->
+        if (File(dir, "build.gradle.kts").exists()) {
+            val dirName = ":extensions:multisrc:${dir.name}"
+            include(dirName)
+            project(dirName).projectDir =
+                File("sources/multisrc/${dir.name}")
+        }
+    }
+} else {
+    // Running in CI (GitHub Actions)
+    val chunkSize = System.getenv("CI_CHUNK_SIZE").toInt()
+    val chunk = System.getenv("CI_CHUNK_NUM").toInt()
+    File(rootDir, "sources").getChunk(chunk,chunkSize) { dir ->
+        if (dir.name != "multisrc") {
+            if (File(dir, "build.gradle.kts").exists()) {
+                val name = ":extensions:individual:${dir.parentFile.name}:${dir.name}"
+                include(name)
+                project(name).projectDir = dir
+            }
+        }
     }
 }
 
@@ -57,5 +71,20 @@ dependencyResolutionManagement {
 inline fun File.eachDir(block: (File) -> Unit) {
     listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
 }
+
+fun File.getChunk(chunk: Int, chunkSize: Int,block: (File) -> Unit) {
+    return listFiles()
+        // Lang folder
+
+        ?.filter { it.isDirectory }
+        // Extension subfolders
+        ?.mapNotNull { dir -> dir.listFiles()?.filter { it.isDirectory } }
+        ?.flatten()
+        ?.sortedBy { it.name }
+        ?.filterNotNull()!!
+        .chunked(chunkSize)[chunk]
+        .forEach { block(it) }
+}
+
 //include(":test-extensions")
 include(":multisrc")
