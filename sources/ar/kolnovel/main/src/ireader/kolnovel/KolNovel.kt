05f5192167f246ab53a57f14ed0fb882e70d8f06
SBS123 <161339329+OtakuArab@users.noreply.github.com>
2024-07-21 08:42:07 +0300

    update KolNovel (#101)
    * Update build.gradle.kts
    
    * Update KolNovel.kt

--- sources/ar/kolnovel/main/src/ireader/kolnovel/KolNovel.kt
+++ sources/ar/kolnovel/main/src/ireader/kolnovel/KolNovel.kt
@@ -20,7 +20,7 @@ abstract class KolNovel(deps: Dependencies) : SourceFactory(
     override val lang: String
         get() = "ar"
     override val baseUrl: String
-        get() = "https://kolbook.xyz"
+        get() = "https://kolcars.shop"
     override val id: Long
         get() = 41
     override val name: String
