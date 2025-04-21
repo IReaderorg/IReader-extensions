4083bb0000941a29adeb8dc5ff9852a2facaa7e3
SBS123 <161339329+OtakuArab@users.noreply.github.com>
2025-04-21 20:16:44 +0300

    Update build.gradle.kts

--- sources/multisrc/skynovel/build.gradle.kts
+++ sources/multisrc/skynovel/build.gradle.kts
@@ -23,7 +23,7 @@ listOf(
     ),
     Extension(
         name = "Hizomanga",
-        versionCode = 1,
+        versionCode = 2,
         libVersion = "1",
         lang = "ar",
         description = "",
