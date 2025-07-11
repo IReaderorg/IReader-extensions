15184833c2d582c7287e5405a2984bb2d3ad897c
SBS123 <161339329+OtakuArab@users.noreply.github.com>
2025-07-11 22:57:10 +0300

    update kolnovel free web (#141)
    * Update build.gradle.kts
    
    * Update KolNovel.kt

--- sources/ar/kolnovel/main/src/ireader/kolnovel/KolNovel.kt
+++ sources/ar/kolnovel/main/src/ireader/kolnovel/KolNovel.kt
@@ -20,7 +20,7 @@ abstract class KolNovel(deps: Dependencies) : SourceFactory(
     override val lang: String
         get() = "ar"
     override val baseUrl: String
-        get() = "https://kolnovel.com"
+        get() = "https://koltry.life"
     override val id: Long
         get() = 41
     override val name: String
