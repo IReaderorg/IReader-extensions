0551fc55bbbc05ced7d6d2173b7e9e402618eaa2
SBS123 <161339329+OtakuArab@users.noreply.github.com>
2025-09-22 02:42:38 +0300

    Create build.gradle.kts

---
+++ sources/ar/realmnovel/build.gradle.kts
@@ -0,0 +1,11 @@
+listOf("ar").map { lang ->
+  Extension(
+    name = "Realmnovel",
+    versionCode = 1,
+    libVersion = "1",
+    lang = lang,
+    description = "",
+    nsfw = false,
+    icon = DEFAULT_ICON
+  )
+}.also(::register)
