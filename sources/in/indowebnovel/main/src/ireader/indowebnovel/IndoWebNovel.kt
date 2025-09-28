65c2505b0b5beb8d80937240d1bfc7dd0dbd5ca9
Mochammad Nopal Attasya <55684192+novalattasya@users.noreply.github.com>
2025-09-28 10:10:49 +0700

    Update Selector and Add Filter

--- sources/in/indowebnovel/main/src/ireader/indowebnovel/IndoWebNovel.kt
+++ sources/in/indowebnovel/main/src/ireader/indowebnovel/IndoWebNovel.kt
@@ -19,7 +19,7 @@ abstract class IndoWebNovel(deps: Dependencies) : SourceFactory(
     override val lang: String
         get() = "en"
     override val baseUrl: String
-        get() = "https://indowebnovel.id/"
+        get() = "https://indowebnovel.id"
     override val id: Long
         get() = 71
     override val name: String
@@ -27,6 +27,17 @@ abstract class IndoWebNovel(deps: Dependencies) : SourceFactory(
 
     override fun getFilters(): FilterList = listOf(
         Filter.Title(),
+        Filter.Sort(
+            "SortBy:",
+            arrayOf(
+                "A-Z",
+                "Z-A",
+                "Latest Update",
+                "Latest Added",
+                "Popular",
+                "Rating"
+            ),
+        )
     )
 
     override fun getCommands(): CommandList = listOf(
@@ -35,23 +46,76 @@ abstract class IndoWebNovel(deps: Dependencies) : SourceFactory(
         Command.Chapter.Fetch(),
     )
 
-
     override val exploreFetchers: List<BaseExploreFetcher>
         get() = listOf(
             BaseExploreFetcher(
-                "Latest",
-                endpoint = "/page/{page}/",
-                selector = ".flexbox3 .flexbox3-item",
+                name = "A-Z",
+                endpoint = "/advanced-search/?page={page}&order=title",
+                selector = ".listupd .bs",
                 nameSelector = "a",
                 nameAtt = "title",
                 linkSelector = "a",
                 linkAtt = "href",
                 coverSelector = "img",
                 coverAtt = "src",
-                maxPage = 10
             ),
             BaseExploreFetcher(
-                "Search",
+                name = "Z-A",
+                endpoint = "/advanced-search/?page={page}&order=titlereverse",
+                selector = ".listupd .bs",
+                nameSelector = "a",
+                nameAtt = "title",
+                linkSelector = "a",
+                linkAtt = "href",
+                coverSelector = "img",
+                coverAtt = "src",
+            ),
+            BaseExploreFetcher(
+                name = "Latest Update",
+                endpoint = "/advanced-search/?page={page}&order=update",
+                selector = ".listupd .bs",
+                nameSelector = "a",
+                nameAtt = "title",
+                linkSelector = "a",
+                linkAtt = "href",
+                coverSelector = "img",
+                coverAtt = "src",
+            ),
+            BaseExploreFetcher(
+                name = "Latest Added",
+                endpoint = "/advanced-search/?page={page}&order=latest",
+                selector = ".listupd .bs",
+                nameSelector = "a",
+                nameAtt = "title",
+                linkSelector = "a",
+                linkAtt = "href",
+                coverSelector = "img",
+                coverAtt = "src",
+            ),
+            BaseExploreFetcher(
+                name = "Popular",
+                endpoint = "/advanced-search/?page={page}&order=popular",
+                selector = ".listupd .bs",
+                nameSelector = "a",
+                nameAtt = "title",
+                linkSelector = "a",
+                linkAtt = "href",
+                coverSelector = "img",
+                coverAtt = "src",
+            ),
+            BaseExploreFetcher(
+                name = "Rating",
+                endpoint = "/advanced-search/?page={page}&order=rating",
+                selector = ".listupd .bs",
+                nameSelector = "a",
+                nameAtt = "title",
+                linkSelector = "a",
+                linkAtt = "href",
+                coverSelector = "img",
+                coverAtt = "src",
+            ),
+            BaseExploreFetcher(
+                name = "Search",
                 endpoint = "/page/{page}/?s={query}",
                 selector = ".flexbox2 .flexbox2-item",
                 nameSelector = "a",
@@ -60,7 +124,6 @@ abstract class IndoWebNovel(deps: Dependencies) : SourceFactory(
                 linkAtt = "href",
                 coverSelector = "img",
                 coverAtt = "src",
-                maxPage = 10,
                 type = SourceFactory.Type.Search
             ),
         )
@@ -70,9 +133,20 @@ abstract class IndoWebNovel(deps: Dependencies) : SourceFactory(
             nameSelector = ".series-title h2",
             coverSelector = ".series-thumb img",
             coverAtt = "src",
+            authorBookSelector = "li:contains(Author) span",
+            categorySelector = "li:contains(Tags) span a",
+            statusSelector = ".series-infoz .status",
+            onStatus = { status ->
+                val lowerStatus = status.lowercase()
+                when {
+                    lowerStatus.contains("ongoing") -> ONGOING
+                    lowerStatus.contains("completed") -> COMPLETED
+                    lowerStatus.contains("hiatus") -> ON_HIATUS
+                    else -> ONGOING
+                }
+            },
             descriptionSelector = ".series-synops p",
         )
-
     override val chapterFetcher: Chapters
         get() = SourceFactory.Chapters(
             selector = ".series-chapter li .flexch-infoz a",
@@ -88,5 +162,4 @@ abstract class IndoWebNovel(deps: Dependencies) : SourceFactory(
             pageTitleSelector = ".title-chapter",
             pageContentSelector = ".text-left p",
         )
-
 }
