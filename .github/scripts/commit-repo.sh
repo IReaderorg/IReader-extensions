6bd60da1c2e565163573d02c970ec9056cf599ff
kazem <kazem.codes@gmail.com>
2025-12-04 14:47:24 +0330

    fix

--- .github/scripts/commit-repo.sh
+++ .github/scripts/commit-repo.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -e
 
-rsync -a --delete --exclude .git --exclude .gitignore ../master/repov2/ .
+# Sync built APKs from master/repo/ to repov2 branch
+# Note: The workflow checks out repov2 branch into 'repo' folder
+# and master branch into 'master' folder
+rsync -a --delete --exclude .git --exclude .gitignore ../master/repo/ .
 git config --global user.email "github-actions[bot]@users.noreply.github.com"
 git config --global user.name "github-actions[bot]"
 git status
 if [ -n "$(git status --porcelain)" ]; then
     git add .
-    git commit -m "Update extensions repov2"
+    git commit -m "Update extensions repo"
     git push
 else
     echo "No changes to commit"
