585b77194132d5cc23cb1b5fab8e1dd033494ab6
kazem <kazem.codes@gmail.com>
2026-01-06 18:25:52 +0330

    fix

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,18 +1,14 @@
-
-
 pluginManagement {
     repositories {
-        gradlePluginPortal()
         google()
         mavenCentral()
+        gradlePluginPortal()
     }
 }
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven(url = "https://maven.google.com")
-        mavenCentral()
         google()
         mavenCentral()
         maven {
