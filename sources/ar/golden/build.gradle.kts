9f1bf5961c31b9773ee9cacec931f27821bd35fa
yemen <hanihameda@gmail.com>
2026-08-09 19:09:44 +0300

    chore(golden): bump versionCode to 3

--- sources/ar/golden/build.gradle.kts
+++ sources/ar/golden/build.gradle.kts
@@ -1,7 +1,7 @@
 listOf("ar").map { lang ->
     Extension(
         name = "Golden",
-        versionCode = 2,
+        versionCode = 3,
         libVersion = "2",
         lang = lang,
         description = "Golden Rest - Arabic Novel Source",
